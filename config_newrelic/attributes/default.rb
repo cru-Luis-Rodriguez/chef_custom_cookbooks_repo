309ebce1990b9c03c3a12087295b36cbae704e92
luis rodriguez <luis.rodriguez@cru.org>
2014-12-01 16:34:25 -0500

    remove config_newrelic to later add as submodule

--- config_newrelic/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-# General settings
-
-default['tomcat-cas']['install_path']='/apps/tomcat-cas'
-
-#Use json on Opsworks Stack setting to provided attribute information. Attributes can not be nil
-
-default['newrelic']['java_agent']['environment']='nil'
-
