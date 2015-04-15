d59b41f2b1b10bfd9c0e5341485b03853e478397
luis rodriguez <luis.rodriguez@cru.org>
2015-04-15 13:12:39 -0400

    add databag support and include library Ec2

--- s3bucket_ops/recipes/default.rb
+++ s3bucket_ops/recipes/default.rb
@@ -7,17 +7,21 @@
 # All rights reserved - Do Not Redistribute
 
 include_recipe 'aws'
+aws = data_bag_item("aws", "main")
+include Opscode::Aws::Ec2
 
 aws_s3_file "/tmp/license.properties" do
   bucket "cru-aem6"
   remote_path "/installation_files/license.properties"
-  #aws_access_key_id node[:custom_access_key]
-  #aws_secret_access_key node[:custom_secret_key]
+  aws_access_key aws['aws_access_key_id']
+  aws_secret_access_key aws['aws_secret_access_key']
+ 
 end
 
 aws_s3_file "/tmp/cq60-author-p4502.jar" do
   bucket "cru-aem6"
   remote_path "/installation_files/cq60-author-p4502.jar"
-  #aws_access_key_id node[:custom_access_key]
-  #aws_secret_access_key node[:custom_secret_key]
+  aws_access_key aws['aws_access_key_id']
+  aws_secret_access_key aws['aws_secret_access_key']
+  
 end
