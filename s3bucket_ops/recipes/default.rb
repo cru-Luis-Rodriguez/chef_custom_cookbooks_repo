4fc2350daa4365487f6a59f65420b4155eb10c27
luis rodriguez <luis.rodriguez@cru.org>
2015-04-15 08:53:41 -0400

    add test for copy additional files

--- s3bucket_ops/recipes/default.rb
+++ s3bucket_ops/recipes/default.rb
@@ -15,3 +15,9 @@ aws_s3_file "/tmp/license.properties" do
   aws_secret_access_key node[:custom_secret_key]
 end
 
+aws_s3_file "/tmp/cq60-author-p4502.jar" do
+  bucket "cru-aem6"
+  remote_path "/installation_files/cq60-author-p4502.jar"
+  aws_access_key_id node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
+end
