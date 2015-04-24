18d9d17107b5f64f1a9a9f62d7ed946a43111f24
luis rodriguez <luis.rodriguez@cru.org>
2015-04-24 00:31:37 -0400

    restore origianl search settings

--- aem/providers/replicator.rb
+++ aem/providers/replicator.rb
@@ -54,8 +54,7 @@ action :add do
   if new_resource.dynamic_cluster
     log "Finding replication hosts dynamically..."
     hosts = []
-     # search(:node, %Q(role:"#{role}" AND aem_cluster_name:"#{cluster_name}")) do |n|
-     search(:node, %Q(role:"#{role}")) do |n|
+     search(:node, %Q(role:"#{role}" AND aem_cluster_name:"#{cluster_name}")) do |n|
         log "Found host: #{n[:fqdn]}"
         hosts << {
           :ipaddress => n[:ipaddress],
