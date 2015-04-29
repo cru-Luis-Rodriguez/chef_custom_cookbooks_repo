c8793ce4ff62567dac4db60c23c5d040ae6362ea
luis rodriguez <luis.rodriguez@cru.org>
2015-04-29 18:18:52 -0400

    update

--- aem/recipes/add_publish_agent.rb
+++ aem/recipes/add_publish_agent.rb
@@ -1,21 +1,20 @@
 author = search(:node, "role:author").first
-Chef::Log.info("author private IP is '#{author[:private_ip]}'")
-Chef::Log.info("author hostname is '#{author[:hostname]}'")
-author_host ="#{author[:private_ip]}"
-instance = "#{node[:hostname]}"
-ipaddress = "#{node[:ipaddress]}"
-port = "#{node['aem']['publish']['port']}"
-user = "#{node['aem']['publish']['admin_user']}"
-password = "#{node['aem']['publish']['admin_password']}"
-name = "#{node[:fqdn]}"
+Chef::Log.info("author private IP is '#{author['private_ip']}'")
+Chef::Log.info("author hostname is '#{author['hostname']}'")
+author_host ="author['private_ip']"
+instance = "node['hostname']"
+ipaddress = "node['ipaddress']"
+port = "node['aem']['publish']['port']"
+user = "node['aem']['publish']['admin_user']"
+password = "node['aem']['publish']['admin_password']"
+name = "node['fqdn']"
 type = "agent"
-local_user = "#{node['aem']['author']['admin_user']}"
-local_password = "#{node['aem']['author']['admin_password']}"
+local_user = "node['aem']['author']['admin_user']"
+local_password = "node['aem']['author']['admin_password']"
 
 
 execute 'add_publish_agent' do
-  command "curl -u #{'local_user'}:#{'local_password'} -X POST http://#{'author_host'}:#{'local_port'}/etc/replication/agents.author/publish-#{'instance'}/_jcr_content -d jcr:title=#{'type'} Agent #{'instance'} -d transportUri=http://#{'ipaddress'}:#{'port'}/bin/receive?sling:authRequestLogin=1 -d enabled=true -d transportUser=#{'user'} -d transportPassword=#{'password'} -d cq:template=/libs/cq/replication/templates/agent -d retryDelay=60000 -d logLevel=info -d serializationType=durbo -d jcr:description=#{'type'} Agent #{'instance'} -d sling:resourceType=cq/replication/components/agent"
+  command "curl -u #{'local_user'}:#{'local_password'} -X POST http://#{'author_host'}:#{'local_port'}/etc/replication/agents.author/publish-cd #{'instance'}/_jcr_content -d jcr:title=#{'type'} Agent #{'instance'} -d transportUri=http://#{'ipaddress'}:#{'port'}/bin/receive?sling:authRequestLogin=1 -d enabled=true -d transportUser=#{'user'} -d transportPassword=#{'password'} -d cq:template=/libs/cq/replication/templates/agent -d retryDelay=60000 -d logLevel=info -d serializationType=durbo -d jcr:description=#{'type'} Agent #{'instance'} -d sling:resourceType=cq/replication/components/agent"
   action :run
 end
-
 
