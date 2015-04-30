2529af99af16dff8c1748faa86a43878370b6b19
luis rodriguez <luis.rodriguez@cru.org>
2015-04-30 15:38:38 -0400

    made the curl command a template to be run with cmd or bash

--- aem/recipes/add_publish_agent.rb
+++ aem/recipes/add_publish_agent.rb
@@ -1,22 +1,28 @@
 author = search(:node, "role:author").first
   Chef::Log.info("author private IP is '#{author['private_ip']}'")
   Chef::Log.info("author hostname is '#{author['hostname']}'")
-  log "Found author host: '#{author['private_dns_name']}'"
-
-author_host ="#{author['private_ip']}"
-instance = "node['hostname']"
-ipaddress = "node['ipaddress']"
-port = "node['aem']['publish']['port']"
-user = "node['aem']['publish']['admin_user']"
-password = "node['aem']['publish']['admin_password']"
-name = "node['fqdn']"
-type = "agent"
-local_user = "node['aem']['author']['admin_user']"
-local_password = "node['aem']['author']['admin_password']"
+  log ("Found author host: '#{author['private_dns_name']}'")
 
 
-execute 'add_publish_agent' do
-  cmd 'curl -u #{local_user}:#{local_password} -X POST http://#{author_host}:#{local_port}/etc/replication/agents.author/publish-cd #{instance}/_jcr_content -d jcr:title=#{type} Agent #{instance} -d transportUri=http://#{ipaddress}:#{port}/bin/receive?sling:authRequestLogin=1 -d enabled=true -d transportUser=#{user} -d transportPassword=#{password} -d cq:template="/libs/cq/replication/templates/agent" -d retryDelay=60000 -d logLevel=info -d serializationType=durbo -d jcr:description=#{type} Agent #{instance} -d sling:resourceType="cq/replication/components/agent"'
-  action :run
+
+template '\tmp\add_publish.sh' do
+  source 'add_publish.erb'
+  owner "root"
+  group "root"
+  mode 0644
+  varialbles(
+    :author_host => author['private_ip'],
+    :instance => node['hostname'],
+    :ipaddress => node['ipaddress'],
+    :port => node['aem']['publish']['port'],
+    :user => node['aem']['publish']['admin_user'],
+    :password => node['aem']['publish']['admin_password'],
+    :local_user => node['aem']['author']['admin_user'],
+    :local_password => node['aem']['author']['admin_password']
+  )
 end
 
+execute 'add_publish.sh' do
+  command '/tmp/add_publish.sh'
+  action :run
+end
